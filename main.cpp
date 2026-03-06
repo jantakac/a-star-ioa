dbe923264e62a54bf455a288620146a12393e208
jantakac <janik.takac@gmail.com>
2026-03-06 19:59:06 +0100

    added minimalistic small gui in QtWidgets

--- main.cpp
+++ main.cpp
@@ -1,68 +1,11 @@
 #include "mainwindow.hpp"
-#include "network.hpp"
-#include "pathfinder.hpp"
 
 #include <QApplication>
-#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    std::vector<Node> sr_network = Network::loadFromFile();
-
-    if (!Network::isContinuous(sr_network)) {
-        std::cout << "The network is not continuous!\n";
-        return 0;
-    }
-
-    std::cout << "The network is continuous\n";
-
-    // -- Dijkstra --
-    uint_fast32_t visitedNodesCountD = 0u;
-    auto startD = std::chrono::steady_clock::now();
-    PathFinder::DistanceAndPath distanceAndPathD = PathFinder::dijkstraFindPath(sr_network,
-                                                                                1,
-                                                                                9995,
-                                                                                visitedNodesCountD);
-    auto endD = std::chrono::steady_clock::now();
-
-    std::cout << "distance: " << distanceAndPathD.first << "\n";
-    std::cout << "path:\n";
-    for (auto &node : distanceAndPathD.second) {
-        std::cout << node << " <- ";
-    }
-    std::cout << "\n";
-    // -- Dijkstra --
-
-    // -- A* --
-    uint_fast32_t visitedNodesCountA = 0u;
-    auto startA = std::chrono::steady_clock::now();
-    PathFinder::DistanceAndPath distanceAndPathA = PathFinder::aStarFindPath(sr_network,
-                                                                             1,
-                                                                             9995,
-                                                                             visitedNodesCountA);
-    auto endA = std::chrono::steady_clock::now();
-
-    std::cout << "distance: " << distanceAndPathA.first << "\n";
-    std::cout << "path:\n";
-    for (auto &node : distanceAndPathA.second) {
-        std::cout << node << " <- ";
-    }
-    std::cout << "\n";
-    // -- A* --
-
-    auto diffD = std::chrono::duration_cast<std::chrono::microseconds>(endD - startD);
-    auto diffA = std::chrono::duration_cast<std::chrono::microseconds>(endA - startA);
-    std::cout << "-----------------------\n";
-    std::cout << "Dijkstra time taken: " << diffD.count() << " microseconds\n";
-    std::cout << "A* time taken: " << diffA.count() << " microseconds\n";
-    std::cout << "-----------------------\n";
-    std::cout << "Dijkstra visited nodes count: " << visitedNodesCountD << "\n";
-    std::cout << "A* visited nodes count: " << visitedNodesCountA << "\n";
-
-    return 0;
-
-    // QApplication a(argc, argv);
-    // MainWindow w;
-    // w.show();
-    // return a.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.hpp"
 #include "./ui_mainwindow.h"
+#include "network.hpp"
+#include "node.hpp"
+#include "pathfinder.hpp"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +14,60 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_runButton_clicked()
+{
+    uint32_t from = ui->fromLineEdit->text().toUInt();
+    uint32_t to = ui->toLineEdit->text().toUInt();
+    ui->textOutput->appendPlainText("Loading files...");
+    std::vector<Node> sr_network = Network::loadFromFile();
+
+    if (!Network::isContinuous(sr_network)) {
+        ui->textOutput->appendPlainText("The network is not continuous!");
+    }
+
+    ui->textOutput->appendPlainText("The network is continuous");
+
+    // -- Dijkstra --
+    uint_fast32_t visitedNodesCountD = 0u;
+    auto startD = std::chrono::steady_clock::now();
+    PathFinder::DistanceAndPath distanceAndPathD = PathFinder::dijkstraFindPath(sr_network,
+                                                                                from,
+                                                                                to,
+                                                                                visitedNodesCountD);
+    auto endD = std::chrono::steady_clock::now();
+    ui->textOutput->appendPlainText(QString("distance: %1\npath:").arg(distanceAndPathD.first));
+    for (auto &node : distanceAndPathD.second) {
+        ui->textOutput->insertPlainText(QString("%1 <- ").arg(node));
+    }
+    ui->textOutput->appendPlainText("");
+    // -- Dijkstra --
+
+    // -- A* --
+    uint_fast32_t visitedNodesCountA = 0u;
+    auto startA = std::chrono::steady_clock::now();
+    PathFinder::DistanceAndPath distanceAndPathA = PathFinder::aStarFindPath(sr_network,
+                                                                             from,
+                                                                             to,
+                                                                             visitedNodesCountA);
+    auto endA = std::chrono::steady_clock::now();
+
+    ui->textOutput->appendPlainText(QString("distance: %1\npath:").arg(distanceAndPathA.first));
+    for (auto &node : distanceAndPathA.second) {
+        ui->textOutput->insertPlainText(QString("%1 <- ").arg(node));
+    }
+    ui->textOutput->appendPlainText("");
+    // -- A* --
+
+    auto diffD = std::chrono::duration_cast<std::chrono::microseconds>(endD - startD);
+    auto diffA = std::chrono::duration_cast<std::chrono::microseconds>(endA - startA);
+    ui->textOutput->appendPlainText("-----------------------");
+    ui->textOutput->appendPlainText(
+        QString("Dijkstra time taken: %1 microseconds").arg(diffD.count()));
+    ui->textOutput->appendPlainText(QString("A* time taken: %1 microseconds").arg(diffA.count()));
+    ui->textOutput->appendPlainText("-----------------------");
+    ui->textOutput->appendPlainText(
+        QString("Dijkstra visited nodes count: %1").arg(visitedNodesCountD));
+    ui->textOutput->appendPlainText(QString("A* visited nodes count: %1").arg(visitedNodesCountA));
 }
